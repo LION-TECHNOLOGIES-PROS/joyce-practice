72f085df19cd1fb81d6c27f5befa94806d08cc23
Lion Tech Pros <info@liontechpros.com>
2021-08-17 15:09:32 +0000

    my eks

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,19 +1,19 @@
-provider "aws" {
+ provider "aws" {
   region = var.region
 }
 
-data "aws_eks_cluster" "cluster" {
+data "aws_eks_cluster" "joyce" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster_auth" "joyce" {
   name = module.eks.cluster_id
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+#  host                   = data.aws_eks_cluster.cluster.endpoint
+  #cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+ # token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
 }
 
